f4cdecb7aa6725dec4536f4637f05a2eb7e39557
Shahadat Hossain <s.hossain1219@gmail.com>
2016-06-10 04:18:03 +0600

    Full name & rewards page & financial report delete

--- app/assets/javascripts/app/pages/registration.js
+++ app/assets/javascripts/app/pages/registration.js
@@ -7,8 +7,5 @@ App.addChild("Registration", {
 
   showPassword: function(event) {
     return Skull.ShowPasswordInput.togglePass('input#user_password', this.$(event.target).prop('checked'));
-  },
-    showPassword: function(event) {
-        return Skull.ShowPasswordConfirmationInput.togglePass('input#user_password_confirmation', this.$(event.target).prop('checked'));
-    }
+  }
 });
